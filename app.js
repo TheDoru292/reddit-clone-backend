5bfbe40de87ce53bc8cbb55496698e2123f33c3e
TheDoru <96445851+TheDoru292@users.noreply.github.com>
2023-06-19 16:33:16 +0200

    app file

---
+++ app.js
@@ -0,0 +1,77 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-fallthrough */
+const http = require('http');
+const express = require('express');
+const debug = require('debug');
+const logger = require('morgan');
+const createError = require('http-errors');
+
+const app = express();
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+const indexRouter = require('./router/index');
+const ErrorInfo = require('./lib/errorInfo');
+
+app.use('/api', indexRouter);
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+app.use(function(err, req, res, next) {
+  console.log(err);
+
+  const Error = new ErrorInfo(err, req.errArray);
+  const errorObj = Error.errorObj;
+
+  res.status(errorObj.statusCode).json({success: false, ...Error.errorObj});
+});
+
+const server = http.createServer(app);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+  
+  if (isNaN(port)) {
+    return val;
+  }
+  
+  if (port >= 0) {
+    return port;
+  }
+  
+  return false;
+}
+  
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  
+  const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+  
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+  case 'EACCES':
+    console.error(bind + ' requires elevated privileges');
+    process.exit(1);
+  case 'EADDRINUSE':
+    console.error(bind + ' is already in use');
+    process.exit(1);
+  default:
+    throw error;
+  }
+}
+  
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
