c0a3b9743cb30a5fd0f5c0398f93c790733984dd
TheDoru <96445851+TheDoru292@users.noreply.github.com>
2023-06-19 16:33:03 +0200

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,19 @@
-// .eslintrc.js example
-modules.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
+module.exports = {
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es2021': true
+  },
+  'extends': 'eslint:recommended',
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest'
+  },
+  'rules': {
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'indent': ['error', 2],
+    'no-multi-spaces': ['error']
   }
+};
