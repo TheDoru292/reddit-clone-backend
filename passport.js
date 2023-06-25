b3c29be13037e9bc65c6ccfe4e0951615edadd2d
TheDoru <96445851+TheDoru292@users.noreply.github.com>
2023-06-25 13:23:21 +0200

    passport file

---
+++ passport.js
@@ -0,0 +1,29 @@
+const conn = require('./services/db');
+const passport = require('passport');
+const passportJWT = require('passport-jwt');
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
+require('dotenv').config();
+
+passport.use(
+  new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET,
+    },
+    (jwtPayload, cb) => {
+      return conn.query(
+        `SELECT * FROM USERS WHERE id = ${jwtPayload.id}`,
+        (err, results) => {
+          if (err) {
+            cb(err);
+          }
+
+          if (results) {
+            cb(null, results[0]);
+          }
+        },
+      );
+    },
+  )
+);
