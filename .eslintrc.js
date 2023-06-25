671425436ef894d2c4a8a779091270ff007fcce6
TheDoru <96445851+TheDoru292@users.noreply.github.com>
2023-06-25 13:16:42 +0200

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,16 @@
 module.exports = {
   'env': {
-    'browser': true,
     'commonjs': true,
-    'es2021': true
+    'node': true,
+    'es2021': true,
   },
   'extends': 'eslint:recommended',
   'overrides': [
   ],
   'parserOptions': {
-    'ecmaVersion': 'latest'
+    'ecmaVersion': 'latest',
   },
   'rules': {
-    'quotes': ['error', 'single'],
-    'semi': ['error', 'always'],
-    'indent': ['error', 2],
-    'no-multi-spaces': ['error']
-  }
+  },
 };
+
