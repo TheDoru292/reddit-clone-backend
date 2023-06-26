33f47b25c37a16b9dde99c2b558dabc0e722be3d
TheDoru <96445851+TheDoru292@users.noreply.github.com>
2023-06-26 09:54:06 +0200

    add subreddit router

--- router/index.js
+++ router/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
+require('../passport');
 
 const authRouter = require('./auth');
 
+const subredditRouter = require('./subbreddit');
+
 router.use('/auth', authRouter);
+
+router.use('/subreddit', subredditRouter);
 
 module.exports = router;
