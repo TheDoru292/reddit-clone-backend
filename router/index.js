7de1ab8b207a1f8947f249c253fdd5839896528f
thedoru <96445851+TheDoru292@users.noreply.github.com>
2023-07-16 15:42:48 +0200

    post router

--- router/index.js
+++ router/index.js
@@ -5,11 +5,14 @@ require("../passport");
 const authRouter = require("./auth");
 const subredditRouter = require("./subbreddit");
 const modRouter = require("./mod");
+const postRouter = require("./post");
 
 router.use("/auth", authRouter);
 
 router.use("/subreddit", subredditRouter);
 
 router.use("/mod", modRouter);
+
+router.use("/post", postRouter);
 
 module.exports = router;
